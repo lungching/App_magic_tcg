9a205af2dfd231121ffb4632eb7ae277803b7a12
lungching <mburns.lungching@gmail.com>
2012-07-21 19:46:33 -0400

    use mechanize query to get info and get description and flavor text

--- lib/AppMagicTCG/Card.pm
+++ lib/AppMagicTCG/Card.pm
@@ -7,30 +7,20 @@ use MagicScrape::Info;
 sub info {
     my $self = shift;
 
-    my (
-        $card_name,
-        $card_description,
-        $card_flavor,
-        $image_path,
-    );
+    my $info;
 
     my $doc_root = '/home/mburns/projects/App_magic_tcg/';  # hard code for now TODO have the server find this
-    $card_name = $self->param('card_name');
+    my $card_name = $self->param('card_name');
 
     if ( $card_name ) {
-        (
-            $card_name,
-            $image_path,
-            $card_description,
-            $card_flavor,
-        ) = MagicScrape::Info::get_general_info( $card_name, $doc_root );
+        $info = MagicScrape::Info::get_general_info( $card_name, $doc_root );
     }
 
     $self->stash(
-        card_name        => $card_name,
-        card_description => $card_description,
-        card_flavor      => $card_flavor,
-        image_path       => $image_path,
+        card_name        => $info->{real_name},
+        card_description => $info->{description},
+        card_flavor      => $info->{flavor},
+        image_path       => $info->{image_path},
     );
 
     $self->render();
--- lib/MagicScrape/Info.pm
+++ lib/MagicScrape/Info.pm
@@ -3,7 +3,10 @@ package MagicScrape::Info;
 use strict;
 use warnings;
 
-use WWW::Mechanize;
+use v5.10;
+use WWW::Mechanize::Query;
+use Data::Dumper;
+use Data::Printer;
 
 my $query_url = 'http://magiccards.info/query?v=card&s=cname&q=';
 
@@ -18,7 +21,7 @@ sub get_general_info {
     $card_entry =~ s/'//g;
     $card_entry = lc($card_entry);
 
-    my $agent = WWW::Mechanize->new();
+    my $agent = WWW::Mechanize::Query->new();
     $agent->get( $query_url . $name );
 
     if ( $agent->content =~ /Your query did not match any cards/ ) {
@@ -27,15 +30,22 @@ sub get_general_info {
     }
 
     my $images = $agent->images();
-    my $image_path = save_image($name, $card_entry, $doc_root, $images );
+    my ($real_name, $image_path) = save_image($name, $card_entry, $doc_root, $images );
     $image_path =~ s/.*(card_images.*)/$1/;
 
-    my $description = get_description( $agent->content() );
-    my $flavor = get_flavor( $agent->content() );
-    my $real_name = $name;
-    # my $real_name = get_name( $agent->content() );
+    # /html/body/table[3]/tr/td[2]/p[2]/b
+    my $container   = $agent->find('html body table')->[3]->find('tr td')->[1];
+    my $description = $container->find('p')->[1]->find('b')->[0]->text;
+    my $flavor      = $container->find('p')->[2]->find('i')->[0]->text;
 
-    return ( $real_name, $image_path, $description, $flavor );
+    my %info = (
+        real_name => $real_name,
+        image_path => $image_path,
+        description => $description,
+        flavor => $flavor,
+    );
+
+    return ( \%info );
 }
 
 sub save_image {
@@ -46,24 +56,22 @@ sub save_image {
         $images,
     ) = @_;
 
-    my $agent = WWW::Mechanize->new();
+    my $agent = WWW::Mechanize::Query->new();
     my ($card_img) = grep { $_->alt =~ /$name/i; } @$images;
 
     warn "could not find card image - " . Dumper($images) unless $card_img;
 
     return undef unless $card_img;
     my $img_url = $card_img->URI;
+    my $real_name = $card_img->alt;
 
     my $file = 'public/card_images/' . $card_entry . '.jpeg';
     my $target_file = $doc_root . $file;
-warn "target file: $target_file";
-    $agent->get( $img_url, ':content_file' => $target_file );
 
-    return $target_file;
+    # only save the file if it doesn't exist already
+    $agent->get( $img_url, ':content_file' => $target_file ) if ! -e $target_file;
+
+    return ($real_name, $target_file);
 }
 
-sub get_description {
-}
-
-sub get_flavor {
-}
+1;
