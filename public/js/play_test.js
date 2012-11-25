d8ca02839079eef6de7ae59d6c44df44addfb4fc
lungching <mburns.lungching@gmail.com>
2012-11-25 11:09:01 -0500

    add button with functionality to draw cards

--- public/js/play_test.js
+++ public/js/play_test.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
 
     var images = $('img[name="library"]');
     library = $.makeArray(images);
-    library.sort(function() { return 0.5 - Math.random() });
+    library = shuffle( library );
     for( var i = 0; i < 7; i++) {
         var card = $( library.shift() );
         card.css('display', 'inline');
@@ -32,3 +32,20 @@ function startMovingCard(event, ui) {
     ui.helper.css('z-index', max_zindex++);
 }
 
+function shuffle(array) {
+    var tmp, current, top = array.length;
+
+    if(top) while(--top) {
+        current = Math.floor(Math.random() * (top + 1));
+        tmp = array[current];
+        array[current] = array[top];
+        array[top] = tmp;
+    }
+
+    return array;
+}
+
+function draw_card() {
+    $( library.shift() ).css('display', 'inline').css('top', '100px').css('left', '15px').css('position', 'absolute').css('z-index', max_zindex);
+    
+}
